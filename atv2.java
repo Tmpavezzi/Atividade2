7be883025ec42a442f83e88f591847f3078dd683
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-04-07 20:51:36 -0300

    Create atv2.java

---
+++ atv2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+
+
+
+
+class Main {
+  public static void main(String[] args) {
+// *Escreva um programa que leia um conjunto de 10 números inteiros positivos. Seu programa deve determinar
+// e imprimir o maior deles. Use o laço FOR 
+    
+    Scanner  entrada = new Scanner(System.in);
+int i ;
+    
+    for( i=0 ;  i<10; i++){
+      System.out.println(i);
+     }
+    System.out.print(i-1);
+    
+    }
+}
